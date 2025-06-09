dd70dfbba578d0b8b6f17ce021b2547c7304c885
davidelettieri <davide.lettieri@gmail.com>
2025-06-09 14:57:43 +0200

    feat: up to 11 (something is off)

--- hittable/material.go
+++ hittable/material.go
@@ -4,6 +4,7 @@ import (
 	"math"
 
 	"github.com/davidelettieri/raytracing-one-weekend-go/ray"
+	"github.com/davidelettieri/raytracing-one-weekend-go/utils"
 	"github.com/davidelettieri/raytracing-one-weekend-go/vec"
 )
 
@@ -72,7 +73,24 @@ func (d Dielectric) Scatter(rIn ray.Ray, rec HitRecord) (ray.Ray, vec.Color, boo
 	}
 
 	unitDirection := rIn.Direction().Unit()
-	refracted := vec.Refract(unitDirection, rec.Normal(), ri)
-	scattered := ray.NewRay(rec.Point(), refracted)
+	cosTheta := math.Min(vec.Dot(unitDirection.Negate(), rec.Normal()), 1.0)
+	sinTheta := math.Sqrt(1.0 - cosTheta*cosTheta)
+
+	var direction vec.Vec3
+	cannotRefract := ri*sinTheta > 1.0
+
+	if cannotRefract || reflectance(cosTheta, ri) > utils.RandomFloat64() {
+		direction = vec.Reflect(unitDirection, rec.Normal())
+	} else {
+		direction = vec.Refract(unitDirection, rec.Normal(), ri)
+	}
+
+	scattered := ray.NewRay(rec.Point(), direction)
 	return scattered, attenuation, true
+}
+
+func reflectance(cosine, refractionIndex float64) float64 {
+	r0 := (1 - refractionIndex) * (1 + refractionIndex)
+	r0 = r0 * r0
+	return r0 + (1-r0)*math.Pow(1-cosine, 5)
 }
--- main.go
+++ main.go
@@ -12,11 +12,13 @@ func main() {
 	materialGround := hittable.NewLambertian(vec.NewColor(0.8, 0.8, 0.0))
 	materialCenter := hittable.NewLambertian(vec.NewColor(0.1, 0.2, 0.5))
 	materialLeft := hittable.NewDielectric(1.5)
+	materialBubble := hittable.NewDielectric(1.0 / 1.5)
 	materialRight := hittable.NewMetal(vec.NewColor(0.8, 0.6, 0.2), 1)
 
 	world.Add(hittable.NewSphere(vec.NewPoint3(0, -100.5, -1), 100, materialGround))
 	world.Add(hittable.NewSphere(vec.NewPoint3(0, 0, -1.2), 0.5, materialCenter))
 	world.Add(hittable.NewSphere(vec.NewPoint3(-1, 0, -1), 0.5, materialLeft))
+	world.Add(hittable.NewSphere(vec.NewPoint3(-1, 0, -1), 0.4, materialBubble))
 	world.Add(hittable.NewSphere(vec.NewPoint3(1, 0, -1), 0.5, materialRight))
 	cam := camera.NewCamera(16.0/9.0, 400, 100, 50)
 	cam.Render(world)
